0658749f09db1896a8de1de78429bd55858be61c
Suraj Shende <surajshende247@gmail.com>
2025-01-06 19:32:55 +0530

    Lec-7 Icons Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { Bluetooth as BluetoothIcon, Home as HomeIcon } from "lucide-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -6,5 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <h1 className="text-red-500">E-Commerce</h1>
+    <HomeIcon size="64" />
+
+    <BluetoothIcon size={100} strokeWidth={1} />
   </>
 );
